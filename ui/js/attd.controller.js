70064aa4e1360444aed409645258ff336ccf25c0
ZNevzz <dsouza.nevil45@gmail.com>
2017-07-06 12:48:00 +0530

    mock objects for attendance add

--- ui/js/attd.controller.js
+++ ui/js/attd.controller.js
@@ -1,11 +1,26 @@
-// Attendance Controller : handles attendance ops
+// Scoreboard Controller : handles Scoreboard ops
 
 
 'use strict';
 
 app.controller('addAttdCtrl',
 function($scope,$filter,$http){		
+	
+	$scope.attendance={
 		
+		'number':'1',
+		'date':''
+		'members':[
+		{'name':'Ker','score':'90','present':false},
+		{'name':'Ver','score':'90','present':false},
+		{'name':'Ber','score':'90','present':false},
+		
+		]
+	}
+	
+	
+	
+	$scope.
 }
 );
 
