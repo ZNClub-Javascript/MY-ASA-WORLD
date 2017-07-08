520512c6ec90d9e535022722d16061f72fb19273
ZNevzz <dsouza.nevil45@gmail.com>
2017-07-08 12:22:01 +0530

    csv & json created

--- src/main/python/format_csv.py
+++ src/main/python/format_csv.py
@@ -11,14 +11,18 @@ import pandas as pd
 
 dir_path='../resources/static/csv/'
 #filenames=['ASA Registration Test Sample.csv']
-filenames=['ASA Registration Test Sample.csv']
+filenames=['ASA Registration Test Sample.xlsx']
 #df = pd.read_csv(dir_path+filenames[0])
+df = pd.read_excel(dir_path+filenames[0],sheetname=1)
+#print(df.head())
 
 ##DROP UNWANTED COL AND RENAME
 
-df = df.drop(['Timestamp'],1)
+df = df.drop(['Is the above number available on WhatsApp ','Timestamp'],1)
 
-df.columns=['name','role','std','school','gender','dob','addr','contact']
+print(df.columns)
+df.columns=['name','role','std','school','gender','dob','addr','contact','group']
+
 
 ##TO CSV
 df.to_csv(dir_path+'ASA-2017.csv',index=False)
