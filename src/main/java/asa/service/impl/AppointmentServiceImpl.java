489b89bde49721fc793aceca2a2f8419d727a922
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-07-12 12:53:27 +0530

    Update AppointmentServiceImpl.java

--- src/main/java/asa/service/impl/AppointmentServiceImpl.java
+++ src/main/java/asa/service/impl/AppointmentServiceImpl.java
@@ -11,8 +11,9 @@ import org.springframework.beans.BeanUtils;
 import java.util.*;
 
 
-@Service("appointmentService")
-public class AppointmentServiceImpl implements ConstantsInterface,AppointmentService{
+//@Service("appointmentService")
+//public class AppointmentServiceImpl implements ConstantsInterface,AppointmentService{
+public class AppointmentServiceImpl implements ConstantsInterface{
 	
 	@Autowired
 	private ScheduleDAO scheduleDAO;		
