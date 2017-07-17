075b387fa707eae0dcacacd22664b6b0e0042228
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-07-17 15:21:49 +0530

    Create AttendanceServiceMock.java

---
+++ src/main/java/asa/service/impl/AttendanceServiceMock.java
@@ -0,0 +1,24 @@
+package asa.service.impl;
+
+import asa.service.ConstantsInterface;
+import asa.service.AttendanceService;
+//import asa.dao.ScheduleDAO ;
+import asa.bean.Attendance;
+//import asa.model.Schedule;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeanUtils;
+import java.util.*;
+
+
+@Service("attendanceService")
+public class AttendanceServiceMock implements ConstantsInterface,AttendanceService{
+	
+	
+	public boolean add(Attendance attendance){
+		
+	return true;
+		
+ 
+	
+}
